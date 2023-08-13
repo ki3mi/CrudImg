2f523da45cb2485e58ad5b1551bd4189bdba5f1c
ki3mi <ccentemejiajosecarlos@gmail.com>
2023-08-13 15:54:20 -0500

    commit inicial

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\EstudianteController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum', 'verified'])->group(function () {
+    Route::resource('/estudiantes', EstudianteController::class);
+    Route::get('/dashboard', function(){
+        return view('dashboard');
+    })->name('dashboard');
+});
